63ab3049c49c5aedac17c520d99b341cf6bf8503
LAPTOP-LAPLOFV5\acer <anunciacion.jay-ar@auf.efu.ph>
2023-03-17 23:39:08 +0800

    first commit

---
+++ Module01-Activity03/Module01-Activity03/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Module01;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Module01_Activity03
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void MyViewResources(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MyViewResources());
+        }
+
+        private async void MyDictionary(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MyDictionary());
+        }
+
+        private async void MyPageResources(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MyPageResources());
+        }
+
+        private async void MyResourceDictionary(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MyResourceDictionary());
+        }
+
+    }
+}
